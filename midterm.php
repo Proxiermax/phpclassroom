b761ba8c6c476945529e4b6a47e0279c7335ebcd
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2021-12-26 22:18:54 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -2,7 +2,7 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 
-  echo "<table border='1'>";
+  echo "<table border='1'align = 'center'>";
   echo"<caption>";
   echo "<h3>";
   echo "<p>";
