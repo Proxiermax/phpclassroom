df46d0b5947a714c823ae4356d76264e3e7c1fe6
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2022-03-14 22:53:12 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(duck.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(Galaxy.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นาย ธีร์ธวัช ศรีบุญ เลขที่ 5 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นาย ธีร์ธวัช ศรีบุญ เลขที่ 5 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
